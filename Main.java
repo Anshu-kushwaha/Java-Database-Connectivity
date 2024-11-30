d8d11e8106027996628447a4f8b8305c7a908d38
Anshu Kushwaha <anshukushwaha112@gmail.com>
2024-11-30 16:02:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+package MyPackage;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student st = new Student();
+        //st.createDatabase();
+        //st.createTable();
+           //CRUD
+        //Create Data
+        //st.createdata();
+        st.readData();
+        //st.update();
+        //st.deleteData();
+
+
+    }
+}
+
